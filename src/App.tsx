1e24a41a0743127226e95b7e813e2328e2037ddd
DamnStraight <ste77236@gmail.com>
2023-02-23 22:02:56 -0500

    Updated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Component } from "solid-js";
 
 import { SubredditProvider } from "./context/SubredditProvider";
-import RedditDeck from "./views/Dashboard/Dashboard";
+import Dashboard from "./views/Dashboard/Dashboard";
 import Navbar from "./views/Navbar/Navbar";
 
 const App: Component = () => {
   return (
     <SubredditProvider>
       <Navbar />
-      <RedditDeck />
+      <Dashboard />
     </SubredditProvider>
   );
 };
